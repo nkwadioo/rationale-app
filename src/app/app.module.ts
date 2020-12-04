60c94ef77b610eeabb05d7cb3b62cafa7678ebbe
Otlotleng <nkwadioo@gmail.com>
2020-12-04 18:43:24 +0200

    header and services initial layouts
    - missing css

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,24 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { SliderComponent } from './slider/slider.component';
+import { ServicesComponent } from './services/services.component';
+import { ProjectsComponent } from './projects/projects.component';
+import { AboutComponent } from './about/about.component';
+import { ContactsComponent } from './contacts/contacts.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    SliderComponent,
+    ServicesComponent,
+    ProjectsComponent,
+    AboutComponent,
+    ContactsComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule
