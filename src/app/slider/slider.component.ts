99d14b0ea6c32b60c706a267d0bf8ba6ef3ef8cd
Otlotleng <nkwadioo@gmail.com>
2020-12-18 15:05:29 +0200

    sliders

--- src/app/slider/slider.component.ts
+++ src/app/slider/slider.component.ts
@@ -6,10 +6,41 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./slider.component.scss']
 })
 export class SliderComponent implements OnInit {
+  slideIndex = 1;
 
   constructor() { }
 
   ngOnInit(): void {
+    this.showSlides(this.slideIndex);
   }
+
+  /* Slideshow JavaScript */
+  
+  
+
+  plusSlides(n) {
+    this.showSlides(this.slideIndex += n);
+  }
+
+  currentSlide(n) {
+    this.showSlides(this.slideIndex = n);
+  }
+
+  showSlides(n) {
+    let i;
+    let slides: any = document.getElementsByClassName("mySlides_2");
+    // let dots: any = document.getElementsByClassName("dot");
+    if (n > slides.length) {this.slideIndex = 1}
+    if (n < 1) {this.slideIndex = slides.length};
+    for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+    }
+    // for (i = 0; i < dots.length; i++) {
+    //   dots[i].classList.remove("active");
+    // }
+    slides[this.slideIndex-1].style.display = "block";
+    // dots[this.slideIndex-1].classList.add("active");
+  }
+/* Slideshow JavaScript */
 
 }
